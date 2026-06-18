bcc862f01728521ec99960dc9be6b5ca9f45e621
Eklavya <rathi.eklavya94121@gmail.com>
2026-06-18 21:52:41 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -11,6 +11,10 @@ public class Main {
 
             String input = scanner.nextLine();
 
+            if (input.equals("exit")) {
+                break;
+            }
+
             System.out.println(input + ": command not found");
         }
     }
